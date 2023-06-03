93e56cbf63e21ee6195d7e09e4745797a3d01a26
kartik paliwal <kartikpaliwal2603@gmail.com>
2023-06-04 00:23:51 +0530

    final

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+const Header = () => {
+	return (
+		<React.Fragment>
+			<header>
+				<h1>
+					<HighlightIcon />Summarily
+				</h1>
+			</header>
+		</React.Fragment>
+	);
+};
+export default Header;
